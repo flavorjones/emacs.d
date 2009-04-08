4ab80121766e4f649945e2561f048977fd4af909
Mike Dalessio <mike@csa.net>
2009-04-08 09:09:11 -0400

    tweaking org-mode customizations

--- custom-file.el
+++ custom-file.el
@@ -13,14 +13,12 @@
  '(js2-enter-indents-newline nil)
  '(js2-mirror-mode nil)
  '(js2-mode-indent-ignore-first-tab t)
- '(org-agenda-files (quote ("~/docs/notes/todo.org" "~/docs/notes/deep_data.org")))
- '(org-drawers (quote ("PROPERTIES" "CLOCK" "CODE")))
+ '(org-agenda-files (quote ("~/docs/notes/todo.org")))
  '(org-file-apps (quote (("txt" . emacs) ("tex" . emacs) ("ltx" . emacs) ("org" . emacs) ("el" . emacs) ("bib" . emacs) ("pdf" . "evince '%s'"))))
  '(org-hide-leading-stars t)
  '(org-log-done (quote time))
  '(org-odd-levels-only t)
  '(org-startup-folded (quote content))
- '(org-todo-keywords (quote ((sequence "TODO" "NEXT" "WAITING" "SOMEDAY" "DONE"))))
  '(safe-local-variable-values (quote ((encoding . utf-8) (ruby-compilation-executable . "ruby") (ruby-compilation-executable . "ruby1.8") (ruby-compilation-executable . "ruby1.9") (ruby-compilation-executable . "rbx") (ruby-compilation-executable . "jruby")))))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
