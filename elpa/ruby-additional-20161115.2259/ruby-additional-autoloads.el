fe6eee686d31129a238bc0e508f81879b17f6e86
Mike Dalessio <mike.dalessio@gmail.com>
2016-12-31 00:41:05 -0500

    added ruby-additional and ruby-refactor packages

---
+++ elpa/ruby-additional-20161115.2259/ruby-additional-autoloads.el
@@ -0,0 +1,65 @@
+;;; ruby-additional-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "rdoc-mode" "rdoc-mode.el" (22631 15094 687859
+;;;;;;  424000))
+;;; Generated autoloads from rdoc-mode.el
+
+(autoload 'rdoc-mode "rdoc-mode" "\
+Major mode for RD editing.
+\\{rdoc-mode-map}
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil "ruby-style" "ruby-style.el" (22631 15094 659859
+;;;;;;  782000))
+;;; Generated autoloads from ruby-style.el
+
+(autoload 'ruby-style-c-mode "ruby-style" "\
+
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil "rubydb2x" "rubydb2x.el" (22631 15094 695859
+;;;;;;  321000))
+;;; Generated autoloads from rubydb2x.el
+
+(autoload 'rubydb "rubydb2x" "\
+Run rubydb on program FILE in buffer *gud-FILE*.
+The directory containing FILE becomes the initial working directory
+and source-file directory for your debugger.
+
+\(fn COMMAND-LINE)" t nil)
+
+;;;***
+
+;;;### (autoloads nil "rubydb3x" "rubydb3x.el" (22631 15094 651859
+;;;;;;  884000))
+;;; Generated autoloads from rubydb3x.el
+
+(autoload 'rubydb "rubydb3x" "\
+Run rubydb on program FILE in buffer *gud-FILE*.
+The directory containing FILE becomes the initial working directory
+and source-file directory for your debugger.
+
+\(fn COMMAND-LINE)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("ruby-additional-pkg.el" "ruby-additional.el")
+;;;;;;  (22631 15094 716675 889000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; ruby-additional-autoloads.el ends here
