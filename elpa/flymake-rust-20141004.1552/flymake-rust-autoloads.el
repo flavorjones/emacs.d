a8a36fdda14c39b14435ad5fe8f2daba400b4d1c
Mike Dalessio <mike.dalessio@gmail.com>
2017-03-01 11:17:43 -0500

    various elpa things
    related to:
    - rust (which I didn't like)
    - elm (cool kids seem to like it)
    - html and markdown (which didn't really work out for me)

---
+++ elpa/flymake-rust-20141004.1552/flymake-rust-autoloads.el
@@ -0,0 +1,16 @@
+;;; flymake-rust-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("flymake-rust.el") (22637 39640 426181
+;;;;;;  18000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; flymake-rust-autoloads.el ends here
