94cc0ecbfd208ce075760650d89f33302115d025
Mike Dalessio <mike.dalessio@gmail.com>
2017-12-02 20:16:58 -0500

    updating go dev environment

---
+++ elpa/go-autocomplete-20170626.323/go-autocomplete-pkg.el
@@ -0,0 +1,1 @@
+(define-package "go-autocomplete" "20170626.323" "auto-complete-mode backend for go-mode" '((auto-complete "1.4.0")) :commit "e990796e68a2ebfc099afed20b99e26d6b64061a" :keywords '("languages"))
