c19899c5e5de7e282d1e06e86bde71529d1ce87e
Mike Dalessio <mike.dalessio@gmail.com>
2017-12-11 09:16:06 -0500

    some cleanup, and improving go mode

--- elpa/flymake-go-20150714.33/flymake-go-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; flymake-go-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("flymake-go.el") (22606 65045 147811 339000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; flymake-go-autoloads.el ends here
--- elpa/flymake-go-20150714.33/flymake-go-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "flymake-go" "20150714.33" "A flymake handler for go-mode files" 'nil :url "https://github.com/robert-zaremba/flymake-go" :keywords '("go" "flymake"))
