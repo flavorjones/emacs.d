c6b259d5486b19cece991da7dfcb4843c27eb503
Mike Dalessio <mike.dalessio@gmail.com>
2019-02-11 02:21:59 -0500

    rufo minor mode

---
+++ elpa/rufo-20170718.1416/rufo-autoloads.el
@@ -0,0 +1,21 @@
+;;; rufo-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "rufo" "rufo.el" (23649 4551 40327 878000))
+;;; Generated autoloads from rufo.el
+
+(autoload 'rufo-minor-mode "rufo" "\
+Use rufo to automatically fix ruby before saving.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; rufo-autoloads.el ends here
---
+++ elpa/rufo-20170718.1416/rufo-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "rufo" "20170718.1416" "use rufo to automatically format ruby files" '((emacs "24.3")) :commit "85a6d80fb05fef396a8029b8f944c92a53faf8fe" :authors '(("Daniel Ma" . "danielhgma@gmail.com")) :maintainer '("Daniel Ma" . "danielhgma@gmail.com") :url "https://github.com/danielma/rufo.el")
