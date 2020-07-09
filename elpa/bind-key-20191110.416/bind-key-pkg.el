37d464fbc96dcc0bb4a7191d8c9ba8f20a511f91
Mike Dalessio <mike.dalessio@gmail.com>
2020-07-09 08:28:06 -0400

    use outline-minor-mode and bicycle in markdown files
    (this is part of an experiment to keep a daily journal in markdown,
    not org)

---
+++ elpa/bind-key-20191110.416/bind-key-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "bind-key" "20191110.416" "A simple way to manage personal keybindings" 'nil :commit "bfc5ecd69cdf71fae0af722d90336c66a6415f03" :keywords '("keys" "keybinding" "config" "dotemacs") :authors '(("John Wiegley" . "johnw@newartisans.com")) :maintainer '("John Wiegley" . "johnw@newartisans.com") :url "https://github.com/jwiegley/use-package")
