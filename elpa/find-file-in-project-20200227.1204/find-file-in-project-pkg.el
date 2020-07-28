088a01e8001dafafd3601f45580ec736051a18e8
Mike Dalessio <mike.dalessio@gmail.com>
2020-07-28 08:26:13 -0400

    use projectile, remove ffip

--- elpa/find-file-in-project-20200227.1204/find-file-in-project-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "find-file-in-project" "20200227.1204" "Find file/directory and review Diff/Patch/Commit efficiently everywhere" '((ivy "0.10.0") (emacs "24.4")) :commit "506f35e91e06463cca7390da6ebffc411b8c220f" :keywords '("project" "convenience") :authors '(("Phil Hagelberg, Doug Alcorn, and Will Farrington")) :maintainer '("Chen Bin" . "chenbin.sh@gmail.com") :url "https://github.com/technomancy/find-file-in-project")
